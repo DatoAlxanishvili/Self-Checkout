cc3e6febbfb9820a541b45963fd83913c31c9b62
vano <v.mekantsishvili@gmail.com>
2015-11-06 19:14:44 +0400

    test

--- SelfCheckout/app/src/main/java/geogroup/selfcheckout/MainActivity.java
+++ SelfCheckout/app/src/main/java/geogroup/selfcheckout/MainActivity.java
@@ -1,7 +1,9 @@
 package geogroup.selfcheckout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
-}
+
+    public void sendName(String st)
+    { Intent intent = new Intent(MainActivity.this,ScanActivity.class);
+        intent.putExtra("marketName", st);
+        startActivity(intent);
+    }
+
+
+    public void onClick(View v) {
+        String marketName = "";
+            switch (v.getId()) {
+                case R.id.smart:
+                    marketName = "smart";
+                    sendName(marketName);
+                    break;
+
+                case R.id.gudvili:
+                    marketName = "gudvili";
+                    sendName(marketName);
+                    break;
+
+                case R.id.ioli:
+                    marketName = "ioli";
+                    sendName(marketName);
+                    break;
+
+                case R.id.nugeshi:
+
+                    break;
+
+                case R.id.oriNabiji:
+
+                    break;
+
+                case R.id.karfuri:
+
+                    break;
+
+            }
+        }
+
+    }
+
---
+++ SelfCheckout/app/src/main/java/geogroup/selfcheckout/ScanActivity.java
@@ -0,0 +1,25 @@
+package geogroup.selfcheckout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by vano on 11/6/2015.
+ */
+public class ScanActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_layout);
+
+        Bundle bundle = getIntent().getExtras();
+        String marketName = bundle.getString("marketName");
+
+
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(marketName);
+
+    }
+}
